6bd330172b4b655465092ec5f5cd4c96160a52cf
Mikko <75028888+MikkoVasankari@users.noreply.github.com>
2024-02-08 13:07:10 +0200

    Update distanceLCD.ino

--- distanceLCD/distanceLCD.ino
+++ distanceLCD/distanceLCD.ino
@@ -4,11 +4,8 @@
 byte triggerPin = 2;
 byte echoPin = 3;
 
-
 // LCD_CONTRAST_PIN = 5;
 // LCD_BACKLIGHT_PIN = 6;
-
-// LiquidCrystal lcd(7, 8, 10, 11, 12, 13);
 
 const int rs = 7, en = 8, d4 = 10, d5 = 11, d6 = 12, d7 = 13;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -16,23 +13,23 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void setup () {
   Serial.begin(9600);
   HCSR04.begin(triggerPin, echoPin);
-  
+
   analogWrite(5, 10);
   analogWrite(6, 50);
   lcd.begin(16, 2);
 
-  // Eka rivi
+  // 1st Line
   lcd.setCursor(0, 0);
   lcd.print("hello world");
-  
-  // Toka rivi
+
+  // 2nd Line
   //lcd.setCursor(0, 1);
   //lcd.print("hello world");
 }
 
 void loop () {
   double* distances = HCSR04.measureDistanceCm();
-  
+
   Serial.print("1: ");
   Serial.print(distances[0]);
   Serial.println(" cm");
